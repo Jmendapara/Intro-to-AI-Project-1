a385a7c236e7681bad6bafe88f8b388e8e52ed6d
Jay Mendapara <jmendapara@gmail.com>
2021-02-15 17:03:25 -0500

    added small change

--- bfsClass.py
+++ bfsClass.py
@@ -11,7 +11,7 @@ class bfsClass:
         self.arr = arr
         self.mazeSize = arr.shape[0]
  
-    def shortestPath(self, startPosition, endPosition, showPathFinderAnimation):
+    def getshortestPath(self, startPosition, endPosition, showPathFinderAnimation):
 
         arr = deepcopy(self.arr)
         mazeSize = self.mazeSize
